be94d57da457717a917a798a20d02405c0ff7cf6
davidfim <zonetdavidzo@gmail.com>
2018-10-28 17:20:59 +0700

    Page ubah Profile

---
+++ assets/js/js1.js
@@ -0,0 +1,40 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+fu	nction showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} // document.getElementById("demo").onclick = function() {tambah()};
+// var angka=document.getElementById("angka");		
+// let i=0;
+// function tambah() {
+// 		i++;
+// 		angka.innerHTML=i;
+// 		console.log(i);
+// 	}
+// function kurang() {
+// 		i--;
+// 		angka.innerHTML=i;
+// 		console.log(i);
+// 	}

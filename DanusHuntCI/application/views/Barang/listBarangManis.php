28f8be25b7dee53e5261137a8ceb270fa6e61bcf
KarimahAzzuhdu12 <kakazuto2912@gmail.com>
2018-12-06 17:57:19 +0700

    get nama barang

--- DanusHuntCI/application/views/Barang/listBarangManis.php
+++ DanusHuntCI/application/views/Barang/listBarangManis.php
@@ -5,16 +5,19 @@
     </div>
     <div class="listkanan">
         <ul>
+            
             <?php foreach ($barang->result_array() as $key): ?>
             <li>
-                <div class="poinbarang">
-                    <img src="<?php echo base_url("images/contohbarang.jpg")?>">
-                    <p><?php echo $key['namaBarang'] ?></p>
-                    <p>RP <?php echo $key['harga'] ?></p>
-                </div>
+                <a href=<?php echo base_url().'detailbarang?barang='.$key['namaBarang']?>>
+                    <div class="poinbarang">
+                        <img src="<?php echo base_url("images/contohbarang.jpg")?>">
+                        <p><?php echo $key['namaBarang'] ?></p>
+                        <p>RP <?php echo $key['harga'] ?></p>
+                    </div>
+                </a>
             </li>
             <?php endforeach ?>
-
+            
         </ul>
 
     </div>
